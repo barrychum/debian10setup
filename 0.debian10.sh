c21977367f7c53b1a2e6f1c636097ee5ba1573c4
barry <barrychum@gmail.com>
2021-06-26 08:22:33 +0800

    init

--- 0.debian10.sh
+++ 0.debian10.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 echo "Please enter new IP"
 read ip
+export if=ens192
 
-sed -i -e '/iface ens32 inet dhcp/ a dns-nameservers 192.168.8.1' /etc/network/interfaces
-sed -i -e '/iface ens32 inet dhcp/ a dns-domain home.local' /etc/network/interfaces
-sed -i -e '/iface ens32 inet dhcp/ a gateway 192.168.8.1' /etc/network/interfaces
-sed -i -e '/iface ens32 inet dhcp/ a network 255.255.255.0' /etc/network/interfaces
-sed -i -e "/iface ens32 inet dhcp/ a address $ip" /etc/network/interfaces
-sed -i -e 's/iface ens32 inet dhcp/iface ens32 inet static/g' /etc/network/interfaces
+sed -i -e "/iface $if inet dhcp/ a dns-nameservers 192.168.38.1" /etc/network/interfaces
+sed -i -e "/iface $if inet dhcp/ a dns-domain home.arpa" /etc/network/interfaces
+sed -i -e "/iface $if inet dhcp/ a gateway 192.168.38.1" /etc/network/interfaces
+sed -i -e "/iface $if inet dhcp/ a network 255.255.255.0" /etc/network/interfaces
+sed -i -e "/iface $if inet dhcp/ a address $ip" /etc/network/interfaces
+sed -i -e "s/iface $if inet dhcp/iface $if inet static/g" /etc/network/interfaces
 
 # if grep -Fxq "ipv6" /etc/sysctl.conf
 if grep -R "#ipv6disabled" /etc/sysctl.conf
 then
   echo "found"
 else
-  sed -i -e "\$anet.ipv6.conf.ens32.disable_ipv6=1" /etc/sysctl.conf
+  sed -i -e "\$anet.ipv6.conf.$if.disable_ipv6=1" /etc/sysctl.conf
   sed -i -e "\$anet.ipv6.conf.lo.disable_ipv6=1" /etc/sysctl.conf
   sed -i -e "\$anet.ipv6.conf.default.disable_ipv6=1" /etc/sysctl.conf
   sed -i -e "\$anet.ipv6.conf.all.disable_ipv6=1" /etc/sysctl.conf
   sed -i -e "\$a#ipv6disabled" /etc/sysctl.conf
 fi
 
-# sed -i -e '/#PermitRootLogin/ a PermitRootLogin yes' /etc/ssh/sshd_config
+# sed -i -e "/#PermitRootLogin/ a PermitRootLogin yes" /etc/ssh/sshd_config
