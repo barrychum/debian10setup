db14f4eee762bcdcf3ce85d206418138a1920bfc
barry <barrychum@gmail.com>
2022-07-02 08:56:30 +0800

    Update 1.install-basic-tools.sh

--- 1.install-basic-tools.sh
+++ 1.install-basic-tools.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-apt install sudo
-apt install open-vm-tools
-apt install sshpass
-apt install openssh-client
-
-apt install dnsutils
+apt install -y sudo \
+open-vm-tools \
+sshpass \
+openssh-client \
+dnsutils
